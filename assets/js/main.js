2278dd708bdc1ada497d445a870eb616a4ebf85a
Williams Hernández <hernandw@gmail.com>
2024-02-08 11:20:08 -0300

    :sparkles: manipulacion del dom

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+let title = document.querySelector("h1");
+
+let subtitle = document.querySelector("#subtitle");
+
+let error = document.querySelector("#error");
+
+title.innerHTML = "Clase de Cocina";
+
+subtitle.innerHTML = "Lista del Supermercado";
+
+error.innerHTML = "soy un error";
+
+let input = document.querySelector("#user");
+
+console.log(input);
+
+console.log(input.value);
+
+let boton = document.querySelector(".btn");
+
+/* boton.setAttribute(
+  "style",
+  "background-color: blue; color: white; padding: 15px"
+); */
+/* boton.style.backgroundColor = "blue" */
+
+let parrafo = document.querySelector('.parrafo')
+
+parrafo.innerHTML = 'Bienvenido a la Academia'
